f99c2daec257b63604152c5bca8de95bfe2b74b9
Danyal Khalid <sdk@SDK-MacBook-Pro.local>
2023-07-18 23:37:46 -0700

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -24,7 +25,31 @@ const resolvers = {
                 .populate('friends')
                 .populate('thoughts');
         },
-    }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+
+            return user;
+        },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPwd = await user.isCorrectPassword(password);
+
+            if (!correctPwd) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
+        },
+    },
 };
 
 module.exports = resolvers;
