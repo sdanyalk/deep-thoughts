379ab73e71915ebcda337629443ca5238e78b5d6
Danyal Khalid <sdk@SDK-MacBook-Pro.local>
2023-07-20 21:13:38 -0700

    added single thought and reactions

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -19,13 +25,37 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route
+                path='/'
+                element={<Home />} />
+              <Route
+                path='/login'
+                element={<Login />} />
+              <Route
+                path='/signup'
+                element={<Signup />} />
+              <Route path="/profile">
+                <Route path=':username' element={<Profile />} />
+                <Route path='' element={<Profile />} />
+              </Route>
+              <Route
+                path="/thought/:id"
+                element={<SingleThought />}
+              />
+              <Route
+                path="*"
+                element={<NoMatch />}
+              />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
