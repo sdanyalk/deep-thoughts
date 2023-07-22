aa67251a86d3dbf1e620629abc11a4ebf5b2aad8
Danyal Khalid <sdk@SDK-MacBook-Pro.local>
2023-07-21 19:41:15 -0700

    added profile navigation

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Auth from '../../utils/auth';
 
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,9 +16,20 @@ const Header = () => {
           <h1>Deep Thoughts</h1>
         </Link>
 
-        <nav className='text-center'>
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>Signup</Link>
+        <nav className="text-center">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
